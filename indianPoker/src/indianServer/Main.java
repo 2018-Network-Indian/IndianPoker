aa8968cfc383c3868300619018b347a27e94600c
k2t2 <37089837+k2t2@users.noreply.github.com>
2018-06-10 00:07:24 +0900

    qq2wfr

---
+++ indianPoker/src/indianServer/Main.java
@@ -0,0 +1,25 @@
+package indianServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+	final static int PORT = 9977;
+	
+	 
+	public static void main(String args[]
+			) throws IOException {
+		
+			ServerSocket socket = new ServerSocket(PORT);
+			while(true) {
+				Socket csock = socket.accept();
+				Accept exe = new Accept(csock);	
+			
+				new Thread(exe).start();
+			} 
+	}
+}
+
+
+
