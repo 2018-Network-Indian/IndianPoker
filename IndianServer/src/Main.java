c1dbd048fdeca885fd49523262718ad3026bf152
k2t2 <37089837+k2t2@users.noreply.github.com>
2018-06-09 23:47:11 +0900

    test

---
+++ IndianServer/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+	final static int PORT = 9977;
+	
+	 
+	public static void main(String args[]
+			) throws IOException {
+		
+			ServerSocket socket = new ServerSocket(PORT);
+			while(true) {
+				Socket csock = socket.accept();
+				Accept exe = new Accept(csock);	
+			
+				new Thread(exe).start();
+			} 
+	}
+}
+
+
+
---
+++ indianClient/src/indianClient/Main.java
@@ -0,0 +1,11 @@
+package indianClient;
+
+import java.net.Socket;
+
+public class Main {
+	
+	public static void main(String[] args) {	
+			LoginFrame test = new LoginFrame(); 
+	} 
+
+}
